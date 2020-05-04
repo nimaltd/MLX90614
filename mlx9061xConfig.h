fbb9e0738ae2905690db82e783341f94ad725ed3
Nima Askari ----- نیما عسکری <nimaltd@yahoo.com>
2020-05-04 13:50:25 +0430

    Delete mlx9061xConfig.h

--- mlx9061xConfig.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MLX9061XCONFIG_H
-#define _MLX9061XCONFIG_H
-
-#define _MLX9061X_I2C         hi2c1
-#define _MLX9061X_I2C_ADDRESS 0xB4
-#define _MLX9061X_FREERTOS    1
-#define _MLX9061X_90614       1             
-#define _MLX9061X_90615       0
-#endif
