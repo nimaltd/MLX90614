8daf923fd22db7d6b01325126a87dca20716303d
Nima Askari ----- نیما عسکری <nimaltd@yahoo.com>
2020-05-04 09:22:03 +0430

    add mlx90614

---
+++ mlx9061xConfig.h
@@ -0,0 +1,9 @@
+#ifndef _MLX9061XCONFIG_H
+#define _MLX9061XCONFIG_H
+
+#define _MLX9061X_I2C         hi2c1
+#define _MLX9061X_I2C_ADDRESS 0xB4
+#define _MLX9061X_FREERTOS    1
+#define _MLX9061X_90614       1             
+#define _MLX9061X_90615       0
+#endif
